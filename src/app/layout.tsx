326d1341120f586bcb09a61698ff658f9db1357a
JJAllanLLC <JJAllanLLC@gmail.com>
2026-02-10 22:14:49 -0500

    Google Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,27 @@ export default function RootLayout({
             }),
           }}
         />
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              id="gtag-js"
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+            />
+            <Script
+              id="gtag-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+                `,
+              }}
+            />
+          </>
+        )}
       </head>
       <body className="font-sans antialiased bg-white text-gray-900">
         <Script
