9ac30d289be8cf792bebae101b6c1d79d5055fd3
JJAllanLLC <JJAllanLLC@gmail.com>
2026-02-12 13:28:05 -0500

    Upgrade next-mdx-remote to v6.0.0 (fixes Vercel security check)

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withMDX from '@next/mdx';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  transpilePackages: ['next-mdx-remote'],
 };
 
 export default withMDX(nextConfig);
