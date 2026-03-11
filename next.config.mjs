cc78d96b836c7e5eefb427dfdc7704fb782dac81
JJAllanLLC <JJAllanLLC@gmail.com>
2026-03-11 18:48:55 -0400

    added redirect to fix 404 error

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ import withMDX from '@next/mdx';
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   transpilePackages: ['next-mdx-remote'],
+  async redirects() {
+    return [
+      {
+        source: '/best-grooming-tools-for-golden-retrievers',
+        destination: '/guides/best-brushes-for-golden-retrievers',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);
